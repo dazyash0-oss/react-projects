8fa82d0ae79d320677de9e05097e210935cceecf
AYESHA <dazyash0@gmail.com>
2026-09-09 15:37:45 +0500

    accordian single selection done

---
+++ accordian/src/components/accordian/data.js
@@ -0,0 +1,24 @@
+const data= [
+    {
+        id : '1',
+        question :'What is an accordion component?',
+        answer : 'An accordion component is a UI element that allows users to toggle the visibility of content sections.',
+    },
+    {
+        id : '2',
+        question : 'How do I use an accordion component?',
+        answer : 'To use an accordion component, you can click on the header of each section to toggle its visibility.',
+    },
+    {
+        id : '3',
+        question : 'What is an accordion component?',
+        answer : 'An accordion component is a UI element that allows users to toggle the visibility of content sections.',
+    },
+     {
+        id : '4',
+        question : 'What is an accordion component?',
+        answer : 'An accordion component is a UI element that allows users to toggle the visibility of content sections.',
+    }
+];
+
+export default data;
