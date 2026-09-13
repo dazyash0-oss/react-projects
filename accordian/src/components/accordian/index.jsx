9d3b287481c901d4eff93c70c47100d577d876c9
AYESHA <dazyash0@gmail.com>
2026-09-14 01:00:51 +0500

    accordian done

--- accordian/src/components/accordian/index.jsx
+++ accordian/src/components/accordian/index.jsx
@@ -17,21 +17,40 @@ export default function Accordion() {
 
     }
 
-    console.log(selected);
+    function handleMultipleSelection(getCurrentId) {
+        let cpyMultiple = [...multiple];
+        const findIndexOfCurrentId = cpyMultiple.indexOf(getCurrentId);
+        console.log(findIndexOfCurrentId);
+
+        if (findIndexOfCurrentId === -1) cpyMultiple.push(getCurrentId)
+
+        else cpyMultiple.splice(findIndexOfCurrentId, 1);
+
+        setMultiple(cpyMultiple);
+    }
+
+    console.log(selected, multiple);
     return (<div className="wrapper">
         <button onClick={() => setEnableMultiSelect(!enableMultiSelect)}>enable multiSelection</button>
         <div className="accordian">
             {data && data.length > 0 ? (
                 data.map(dataItem => (
                     <div className="item">
-                        <div onClick={() => handleSingleSelection(dataItem.id)} className="title ">
+                        <div onClick={enableMultiSelect ?
+                            () => handleMultipleSelection(dataItem.id)
+                            : () => handleSingleSelection(dataItem.id)
+                        }
+                            className="title ">
                             <h3>
                                 {dataItem.question}
                             </h3>
                             <span>+</span>
                         </div>
                         {
-                            selected === dataItem.id ?
+
+                        }
+                        {
+                            selected === dataItem.id || multiple.indexOf(dataItem.id) !== -1 ?
                                 <div className="content">{dataItem.answer}</div>
 
 
